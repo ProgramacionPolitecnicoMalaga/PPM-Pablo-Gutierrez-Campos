95e36af53fa9f6cba6ab9392786fb5f445c9a13a
Pablo1507 <55407957+Pablo1507@users.noreply.github.com>
2020-05-21 17:10:21 +0200

    1 commit examen

---
+++ Examen Programación - 1/Examen_Patrones/src/com/politecnicomalaga/strategy/Criterios/IntentarCoger.java
@@ -0,0 +1,8 @@
+package com.politecnicomalaga.strategy.Criterios;
+
+public class IntentarCoger implements SelectorDeseo {
+    @Override
+    public String elegirDeseo(int numero) {
+        return null;
+    }
+}
---
+++ Examen Programación - 1/Examen_Patrones/src/com/politecnicomalaga/strategy/Criterios/LlorarYGritar.java
@@ -0,0 +1,8 @@
+package com.politecnicomalaga.strategy.Criterios;
+
+public class LlorarYGritar implements SelectorDeseo {
+    @Override
+    public String elegirDeseo(int numero) {
+        return null;
+    }
+}
